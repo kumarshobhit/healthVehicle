6f5b22d8dddb3a31c0ef7b4cc76773869151404d
Udit-takkar <53316345+Udit-takkar@users.noreply.github.com>
2021-05-30 07:23:49 +0530

    Driver Dasboard and Profile

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const User = require("../models/user");
 const Ambulance = require("../models/ambulance");
 const Booking = require("../models/bookings");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    user: 'ksushant6566@gmail.com',
-    pass: 'Sushant@6566'
-  }
+    user: "ksushant6566@gmail.com",
+    pass: "Sushant@6566",
+  },
 });
 
 router.get("/", (req, res) => {
@@ -49,41 +49,70 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
+// To get Booking for Driver
+router.get("/driver/:id", async (req, res, next) => {
+  // obtain booking id from request parameters
+  const id = req.params.id;
+
+  // get booking by id from database
+
+  try {
+    const ambulance = await Ambulance.findOne({ _id: id });
+    const bookings = await Booking.findOne({ ambulance: id });
+    const user = await User.findOne({ _id: bookings.user });
+    const response = {
+      booking: {
+        _id: bookings._id,
+        userCoordinates: bookings.userCoordinates,
+        ambulanceCoordinates: bookings.ambulanceCoordinates,
+        user,
+        ambulance,
+        bookedtime: bookings.bookedtime,
+        status: bookings.status,
+      },
+    };
+    res.status(200).json({ response });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 router.post("/createBooking", async (req, res) => {
   try {
-
-    const ambulance = await Ambulance.findOne({ _id: req.body.ambulance});
-    const user = await User.findOne({ _id: req.body.user});
+    const ambulance = await Ambulance.findOne({ _id: req.body.ambulance });
+    const user = await User.findOne({ _id: req.body.user });
 
     const booking = new Booking({
       ...req.body,
       user: user,
-      ambulance: ambulance
+      ambulance: ambulance,
     });
-    
-    booking.save()
-    .then(async (booking) => {
-      await Ambulance.updateOne({ _id: req.body.ambulance },{ $set: { available: false } } );
+
+    booking.save().then(async (booking) => {
+      await Ambulance.updateOne(
+        { _id: req.body.ambulance },
+        { $set: { available: false } }
+      );
       console.log(booking);
       const response = {
         booking: booking,
       };
-    
+
       const mailOptions = {
-        from: 'ksushant6566@gmail.com',
+        from: "ksushant6566@gmail.com",
         to: `${ambulance.email}`,
-        subject: 'URGENT: New Booking Request',
-        text: `Dear ${ambulance.name}\n${user.firstName} has requested an ambulance please report.\n Check your dashboard for more details`
+        subject: "URGENT: New Booking Request",
+        text: `Dear ${ambulance.name}\n${user.firstName} has requested an ambulance please report.\n Check your dashboard for more details`,
       };
-          
+
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           console.log(error);
-          res.setHeader("Access-Control-Allow-Origin", "*") 
-          res.json("unsuccessfull")
+          res.setHeader("Access-Control-Allow-Origin", "*");
+          res.json("unsuccessfull");
         } else {
-            console.log('Email sent: ' + info.response);
-            res.json("successfull")
+          console.log("Email sent: " + info.response);
+          res.json("successfull");
         }
       });
 
--- frontend/src/features/booking/Booking.js
+++ frontend/src/features/booking/Booking.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const initialState = {
   loading: false,
   error: null,
-  user: null,
+  user: {},
   ambulance: {},
   userCoordinates: [],
   ambulanceCoordinates: [],
@@ -12,7 +12,12 @@ const initialState = {
   status: "pending",
 };
 
-const createurl = "https://covihelp-india.herokuapp.com/api/bookings/createBooking";
+const createurl =
+  "https://covihelp-india.herokuapp.com/api/bookings/createBooking";
+const getDriverBookingURL =
+  "https://covihelp-india.herokuapp.com/api/bookings/driver/";
+const localgetDriverBookingurl = "http://localhost:8000/api/bookings/driver/";
+
 export const createbooking = createAsyncThunk(
   "booking/createbooking",
   async (
@@ -39,6 +44,26 @@ export const createbooking = createAsyncThunk(
   }
 );
 
+export const getDriverBooking = createAsyncThunk(
+  "booking/getBooking",
+  async (id, { rejectWithValue }) => {
+    console.log("reached thunk");
+    try {
+      // const res = await axios.get(`${localgetDriverBookingurl}${id}`);
+      const res = await axios.get(
+        `${localgetDriverBookingurl}60abf2d9ab45d00328e7ce87`
+      );
+
+      console.log(res.data.response);
+
+      return res.data.response;
+    } catch (e) {
+      console.log(e.response.data);
+      return rejectWithValue(e.response.data.msg);
+    }
+  }
+);
+
 export const bookingSlice = createSlice({
   name: "booking",
   initialState,
@@ -48,7 +73,7 @@ export const bookingSlice = createSlice({
       Object.assign(state, {
         loading: false,
         error: null,
-        user: null,
+        user: {},
         ambulance: {},
         userCoordinates: [],
         ambulanceCoordinates: [],
@@ -74,7 +99,45 @@ export const bookingSlice = createSlice({
       Object.assign(state, {
         loading: false,
         error: null,
-        user: null,
+        user: {},
+        ambulance: {},
+        userCoordinates: [],
+        ambulanceCoordinates: [],
+        bookedtime: null,
+        status: "pending",
+      });
+    },
+    [getDriverBooking.pending]: (state, action) => {
+      Object.assign(state, {
+        loading: false,
+        error: null,
+        user: {},
+        ambulance: {},
+        userCoordinates: [],
+        ambulanceCoordinates: [],
+        bookedtime: null,
+        status: "pending",
+      });
+    },
+    [getDriverBooking.fulfilled]: (state, action) => {
+      console.log(action.payload);
+      Object.assign(state, {
+        loading: false,
+        error: null,
+        user: action.payload.booking.user,
+        ambulance: action.payload.booking.ambulance,
+        userCoordinates: action.payload.booking.userCoordinates,
+        ambulanceCoordinates: action.payload.booking.ambulanceCoordinates,
+        bookedtime: action.payload.booking.bookedtime,
+        status: "booked",
+      });
+    },
+    [getDriverBooking.rejected]: (state, action) => {
+      console.log(action.payload);
+      Object.assign(state, {
+        loading: false,
+        error: null,
+        user: {},
         ambulance: {},
         userCoordinates: [],
         ambulanceCoordinates: [],
@@ -91,3 +154,5 @@ export const ambulanceCoordinates = (state) =>
   state.booking.ambulanceCoordinates;
 export const getBookingStatus = (state) => state.booking.status;
 export const getAmbulance = (state) => state.booking.ambulance;
+export const getUser = (state) => state.booking.user;
+export const getBookedtime = (state) => state.booking.bookedtime;
